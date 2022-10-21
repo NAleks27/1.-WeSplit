089508c0ab9fc6f52b6786a24ca259a9cc99d226
NAleks27 <aleksey.dev27@gmail.com>
2022-10-21 16:22:23 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aleksey Nosik on 25.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
